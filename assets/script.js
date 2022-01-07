fb4684d16bf1d4cea34214bba73c49447a8ae9c7
isaac petersen <isaac.petersen5@gmail.com>
2022-01-06 18:40:12 -0800

    everything's broken now

--- assets/script.js
+++ assets/script.js
@@ -12,30 +12,25 @@ var textContent;
 //saving text content with Local storage
 function save(event) {
     event.preventDefault();
-    //This line was created by the help of Chrissie Spalding. JEsUS made my brain hurt.
-    textContent = $(event.target).siblings().eq(1).val();
-    localStorage.setItem('textContent', JSON.stringify(textContent));
-    console.log(event)
+
 }
 
 //loading text to it's appropriate box with local storage
 function load() {
-    textContent = JSON.parse(localStorage.getItem('textContent'));
-    for (var i = 0; i < 9; i++) {
-        taskList.children().eq(i).children().eq(1).text(taskList[i]);
+
 };
-}
+
 
 //updating time using moment and walking the dom using query
 //setting variable time to check if each child using i is equal to the current time
 function update() {
-    for (var i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
         //Adding 9 to the end of time to match the actual hours of the day. If not, this would think the 9am slot was 0 the 10am was 1, etc.
         var time = taskList.children().eq(i).children().eq(2).data('time') + 9;
         console.log(time)
         if (time < timeNow) {
             taskList.children().eq(i).children().eq(1).addClass('past');
-        } else if (time === timeNow) {
+        } else if (time == timeNow) {
             taskList.children().eq(i).children().eq(1).addClass('present');
         } else {
             taskList.children().eq(i).children().eq(1).addClass('future')
